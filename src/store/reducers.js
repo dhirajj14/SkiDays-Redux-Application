60e6389c36f7486e989f0a63bde607cce63c39e9
dhirajj14 <djain14@hawk.iit.edu>
2020-07-07 14:51:51 -0500

    improve code syntax

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,17 +1,13 @@
 import C from '../constants'
 
-export const goal = (state, action) => {
-    if(action.type === C.SET_GOAL){
-        return parseInt(action.payload)
-    }else{
-        return state
-    }
-}
+export const goal = (state, action) => 
+    (action.type === C.SET_GOAL)
+       ? parseInt(action.payload) :
+       state
+    
 
-export const skiDay = (state=null, action) =>{
-    if(action.type === C.ADD_DAY){
-        return action.payload
-    }else{
-        return state
-    }
-}
+
+export const skiDay = (state=null, action) =>
+   (action.type === C.ADD_DAY)
+    ? action.payload :
+    state
