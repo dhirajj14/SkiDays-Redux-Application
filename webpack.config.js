045754f525a81cab6090fc3ae42e34fed8a17cd9
dhirajj14 <djain14@hawk.iit.edu>
2020-07-11 11:27:18 -0500

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: "/dist/assets",
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: "./dist",
+		port: 3000
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: 'babel-loader'
+                
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+			}
+		]
+	}
+}
