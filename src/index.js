8238bcc0d6b1862934ba8dfaf63c4c636fb8128f
dhirajj14 <djain14@hawk.iit.edu>
2020-07-11 12:06:10 -0500

    unsubscribe store

--- src/index.js
+++ src/index.js
@@ -6,27 +6,30 @@ import { createStore } from 'redux'
 const initialState = localStorage['redux-store'] ? JSON.parse(localStorage['redux-store']) : {}
 let store = createStore(appReducer)
 
-store.subscribe(() => console.log("Initial State", store.getState()))
+//store.subscribe(() => console.log("Initial State", store.getState()))
 
 window.store = store
 
-store.subscribe(() => {
-    const state = JSON.stringify(store.getState())
-    localStorage['redux-store'] = state
+const unsubscribeGoalLoagger = store.subscribe(() => {
+    console.log(`Goal: ${store.getState().goal}`)
 })
 
 
-store.dispatch({
-    type: C.ADD_DAY,
-    payload: {
-        "resort" : "DJ Wood",
-        "date" : "2020-07-09",
-        "powder" : false,
-        "backcountry" : true
-    }
-})
+// store.dispatch({
+//     type: C.ADD_DAY,
+//     payload: {
+//         "resort" : "DJ Wood",
+//         "date" : "2020-07-09",
+//         "powder" : false,
+//         "backcountry" : true
+//     }
+// })
 
-store.dispatch({
-    type: C.SET_GOAL,
-    payload: 2
-})
+setInterval(()=>{
+    store.dispatch({
+        type: C.SET_GOAL,
+        payload: Math.floor(Math.random() * 100)
+    })
+}, 250)
+
+setTimeout(()=> unsubscribeGoalLoagger, 3000)
